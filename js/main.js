bf3ba00400fe305109c3af99320850652bf82bcd
Victoria Abramova <barkovavika89@gmail.com>
2024-05-27 19:04:10 +0300

    add feedback form on 2nd page

--- js/main.js
+++ js/main.js
@@ -24,10 +24,56 @@ function toggleMenu() {
 toggleMenu()
 
 // для второй страницы
-const buttons = document.querySelectorAll("#faqToggle");
+const buttons = document.querySelectorAll('#faqToggle');
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () =>
-    button.parentElement.classList.toggle("active")
+  button.addEventListener('click', () =>
+    button.parentElement.classList.toggle('active')
   );
 });
+
+//для формы отправки
+const form = document.querySelector('form')
+const email = document.getElementById('email')
+const mess = document.getElementById('message')
+
+function sendEmail() {
+  const bodyMessage = `Почта отправителя: ${email.value} <br> Текст сообщения: <br> ${mess.value}`
+
+  Email.send({
+    Host : 'smtp.elasticemail.com',
+    Username : 'barkovavika89@gmail.com',
+    Password : 'BD36661DD4DD085C0FB5D55D50A6FA9DF652',
+    To : 'barkovavika89@gmail.com',
+    From : 'barkovavika89@gmail.com',
+    Subject : 'Письмо от чайной IROH',
+    Body : bodyMessage
+  }).then(
+    message => {
+      if (message == 'OK') {
+        Swal.fire({
+          title: 'Успешно!',
+          text: 'Ваше сообщение отправлено, ожидайте ответа!',
+          icon: 'success'
+        });
+      }
+    }
+  );
+}
+
+// function checkEmail() {
+//   const emsilRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+//   if (!email.value.match(emailRegex)) {
+//     email.classList.add('text-red')
+//   }
+//   else {
+//     email.classList.remove('text-red')
+//   }
+// }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // checkEmail()
+  sendEmail()
+})
